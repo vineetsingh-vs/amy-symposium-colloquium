790870fda04a0ced5256fa4ccf7a43c67e214394
Ethan Barr <ej.barr00@gmail.com>
2022-03-29 16:12:49 -0500

    Adding Routes

--- colloquium/src/App.jsx
+++ colloquium/src/App.jsx
@@ -8,8 +8,31 @@ function App() {
         <Router>
             <div className="mainContainer">
                 <Switch>
-                    <Route exact path="/" component={AdminDashboard} />
+                    {/* Admin Page of Users */}
+                    <Route exact path="/admin" component={AdminDashboard} />
                     <Route path="*" component={PageNotFound} />
+
+                    {/* Log in Page */}
+                    {/* <Route exact path="/" components={LoginView} /> */}
+
+                    {/* Document and Comments */}
+                    {/* <Route exact path="/:paperId" component={DocumentView} /> */}
+
+                    {/* Review Page containing all Comments + overall Reviews */}
+                    {/* <Route exact path="/:paperId/reviews" component={ReviewView} /> */}
+
+                    {/* <Route exact path="/:paperId/comments" component={CommentView} /> */}
+
+                    {/* Main User Screens */}
+                    {/* <Route exact path="/published" component={PublishView} /> */}
+                    {/* <Route exact path="/shared" component={SharedView} /> */}
+                    {/* <Route exact path="/mypapers" component={MyPapersView} /> */}
+
+                    {/* "Popup screens" */}
+                    {/* <Route exact path="/:paperId/share" components={SharingView} /> */}
+                    {/* <Route exact path="/:paperId/reupload" components={ReuploadView} /> */}
+                    {/* <Route exact path="/upload" components={UploadPaperView} /> */}
+
                 </Switch>
             </div>
         </Router>
