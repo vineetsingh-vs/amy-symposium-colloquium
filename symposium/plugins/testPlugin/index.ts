d62c0424b592b7f8b327bdad7e10e96a2a70025b
jayo78 <jhurst1@terpmail.umd.edu>
2022-04-28 14:32:51 -0400

    add test emits in testPlugin

--- symposium/plugins/testPlugin/index.ts
+++ symposium/plugins/testPlugin/index.ts
@@ -1,15 +1,36 @@
-import express, { Application, Request, Response } from "express";
+import { Application } from "express";
 import { emitter } from "../../src/emitter";
 
 const testPlugin = {
-    //
-    // initialize plugin
     initPlugin: (app: Application) => {
+        console.log("testPlugin init");
+
         //
         // set up event listeners
         emitter.on("paperCreated", (args) => {
-            console.log("emitter: paper created: ");
+            console.log("emitter test: paper created: ");
             console.log(args.paper.title);
+        });
+        emitter.on("paperUpdated", (args) => {
+            console.log("emitter test: paper updated: ");
+            console.log(args.paper.title);
+        });
+        emitter.on("paperRequested", (args) => {
+            console.log("emitter test: paper requested: ");
+            console.log(args.req.params);
+            console.log(args.paper.title);
+        });
+        emitter.on("commentCreated", (args) => {
+            console.log("emitter test: comment created: ");
+            console.log(args.comment.content);
+        });
+        emitter.on("commentRequested", (args) => {
+            console.log("emitter test: comment requested: ");
+            console.log(args.comment.content);
+        });
+        emitter.on("commentUpdated", (args) => {
+            console.log("emitter test: comment updated: ");
+            console.log(args.comment.content);
         });
     },
 };
